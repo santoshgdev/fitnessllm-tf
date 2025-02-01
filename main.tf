bf27a79e8964231a3a01cf9e694c21817691971b
Santosh Gummidipundi <santosh@santoshg.io>
2025-01-31 18:24:04 -0800

    remove sensitive info

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
-variable "credentials_path" {}
+#These variables are set via TF_VAR_* environmental variables
+variable "credentials_path" {} # PATH TO CREDENTIALS PATH
+variable "project" {} # PROJECT_NAME
 
 provider "google" {
   credentials = file("${var.credentials_path}")
-  project = "stable-splicer-447620-b4"
+  project = "${var.project}"
   region = "us-west1"
   zone = "us-west1-a"
 }
